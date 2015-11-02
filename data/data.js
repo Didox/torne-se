3f63af1e14ae372d27d7f9ce2214dca3e8b73e10
Didox <dansantos@abril.com.br>
2015-11-02 13:18:21 -0200

    data

---
+++ data/data.js
@@ -0,0 +1,37 @@
+var data = [
+	{
+		titulo: 'Aula 1',
+		descricao: 'Blablabla',
+		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+	},
+	{
+		titulo: 'Aula 2',
+		descricao: 'Blablabla',
+		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+	},
+	{
+		titulo: 'Aula 3',
+		descricao: 'Blablabla',
+		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+	},
+	{
+		titulo: 'Aula 4',
+		descricao: 'Blablabla',
+		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+	},
+	{
+		titulo: 'Aula 5',
+		descricao: 'Blablabla',
+		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+	},
+	{
+		titulo: 'Aula 6',
+		descricao: 'Blablabla',
+		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+	},
+	{
+		titulo: 'Aula 7',
+		descricao: 'Blablabla',
+		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+	},
+]
