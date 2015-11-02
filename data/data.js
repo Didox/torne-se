db4a03fccb6be07df1f96ad09ee8fbbc1b6ef4b3
Didox <dansantos@abril.com.br>
2015-11-02 14:03:19 -0200

    data

--- data/data.js
+++ data/data.js
@@ -5,38 +5,53 @@ var version = {
 
 var data = [
 	{
-		titulo: 'Aula 1',
-		descricao: 'Blablabla',
+		titulo: 'Aula 8 - Funções recursivas e constantes',
+		descricao: 'Nesta aula você irá aprender o que é uma variável constante e como aplicar em um programa, também irá aprender o que é funções recursivas, como utiliza-las e como aplicar em um programa.',
 		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
 	},
 	{
-		titulo: 'Aula 2',
-		descricao: 'Blablabla',
-		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+		titulo: 'Aula 8 - Funções',
+		descricao: 'Nesta aula você irá aprender como criar uma função em javascript, para que serve e quais os tipos de funções utilizadas. ',
+		videoYoutube: 'https://www.youtube.com/watch?v=WGbVQTWqEko'
 	},
 	{
-		titulo: 'Aula 3',
-		descricao: 'Blablabla',
-		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+		titulo: 'Aula 7 - Hash',
+		descricao: 'Nesta aula você irá aprender a utilizar Hash, ou seja uma forma estrutura para organizar suas variáveis como um objeto ',
+		videoYoutube: 'https://www.youtube.com/watch?v=SCL93-7zWWw'
 	},
 	{
-		titulo: 'Aula 4',
-		descricao: 'Blablabla',
-		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+		titulo: 'Aula 6 - Array',
+		descricao: 'Nesta aula você irá aprender como utilizar o Array (coleção de espaços de memória), verá na prática a sua utilização e criará junto comigo um programa para calcular a média de alunos. ',
+		videoYoutube: 'https://www.youtube.com/watch?v=e3r9Q9L0-sU'
 	},
 	{
-		titulo: 'Aula 5',
-		descricao: 'Blablabla',
-		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+		titulo: 'Aula 5 - Looping',
+		descricao: 'Nesta aula ensino como utilizar a estrutura de repetição (lopping) e a diferença de um programa utilizando repetição e não usando repetição. ',
+		videoYoutube: 'https://www.youtube.com/watch?v=XwmFtcPanIU'
 	},
 	{
-		titulo: 'Aula 6',
-		descricao: 'Blablabla',
-		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+		titulo: 'Aula 4 - Operadores lógicos parte 2',
+		descricao: 'Nesta aula iremos falar sobre operadores condicionais, concatenação de strings, diferença de variável string e inteiro, operadores lógicos ',
+		videoYoutube: 'https://www.youtube.com/watch?v=nYF_WhoexhY'
 	},
 	{
-		titulo: 'Aula 7',
-		descricao: 'Blablabla',
-		videoYoutube: 'https://www.youtube.com/watch?v=NYGGaTd_xZk'
+		titulo: 'Aula 4 - Operadores lógicos parte 1',
+		descricao: 'Nesta aula iremos falar sobre operadores condicionais, concatenação de strings, diferença de variável string e inteiro, operadores lógicos ',
+		videoYoutube: 'https://www.youtube.com/watch?v=cN_F2E7yKVQ'
+	},
+	{
+		titulo: 'Aula 3 - Operadores matemáticos',
+		descricao: 'Nesta aula iremos aprender como utilizar a lógica de programação para resolver expressões numéricas, utilizando os operadores matemáticos. ',
+		videoYoutube: 'https://www.youtube.com/watch?v=z-_vVAyJLrY'
+	},
+	{
+		titulo: 'Aula 2 - Variáveis',
+		descricao: 'Este vídeo mostra o que é uma variável, para que serve uma variável e como utilizar uma variável em um código fonte ',
+		videoYoutube: 'https://www.youtube.com/watch?v=5_ak7LNFyWw'
+	},
+	{
+		titulo: 'Aula 1 - Conceitos básicos',
+		descricao: 'Aula 1 que fala sobre os conceitos básicos de como ser um programador',
+		videoYoutube: 'https://www.youtube.com/watch?v=oHJzyf3EwWU'
 	},
 ]
