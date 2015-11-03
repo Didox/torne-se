ba5e6861a75556c42ec82120b2ca62d44dff75fc
Didox <didox_59@yahoo.com.br>
2015-11-03 10:20:41 -0200

    Programação funcional

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@ var version = {
 }
 
 var aviso = {
-  message: 'Quarta-feira está chegando pessoal, aproveitem e revisem as aulas, logo vem mais por aí.'
+  message: 'Quarta-feira está chegando pessoal, aproveitem e revisem as aulas, logo vem mais por aí. Tema próxima aula: programação funcional'
 }
 
 var data = [
