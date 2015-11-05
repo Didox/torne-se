76c78e64bea2f7c5ff165e9e7c669aaf0681e025
Didox <dansantos@abril.com.br>
2015-11-05 07:54:20 -0200

    fixes

--- torne-se-application/Resources/HTML/js/api.js
+++ torne-se-application/Resources/HTML/js/api.js
@@ -1,6 +1,6 @@
 var app = window.app || {};
 
-app.appVersion       = 2;
+app.appVersion       = 3;
 app.updateHost       = "https://rawgit.com/Didox/torne-se/master/data/data.js";
 
 app.showLoading = function(){
