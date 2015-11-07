8fdb895b0d37efa2aa7d556ed3de4a74161e830c
Didox <dansantos@abril.com.br>
2015-11-07 01:08:50 -0200

    refectore

--- torne-se-application/Resources/includes/application.js
+++ torne-se-application/Resources/includes/application.js
@@ -3,7 +3,7 @@ application.win = utils.createWindow(true);
 
 application.webview = Ti.UI.createWebView({
   title: 'Torne-se um programador',
-  url: 'index.html',
+  url: '/index.html',
   willHandleTouches:false,
   enableZoomControls:false,
   backgroundColor: '#333'
