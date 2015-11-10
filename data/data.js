0f11a9e64e3c08b10d40914a78bc291502e1a178
Didox <didox_59@yahoo.com.br>
2015-11-10 21:04:07 -0200

    sem

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@ var version = {
 }
 
 var aviso = {
-  message: 'Fiquem ligados, nova aula amanhã, tema: Orientação a objetos',
+  message: '',
   confirm: false,
   url: 'video.html?id=DbR-luEtC1I',
   openUrl: false
