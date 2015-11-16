736dc4053100b2cfeee071c5f6cb6489ba0557d2
Didox <dansantos@abril.com.br>
2015-11-16 00:14:19 -0200

    vagas

--- torne-se-site/app/routes/index.js
+++ torne-se-site/app/routes/index.js
@@ -3,11 +3,14 @@ var router = express.Router();
 var HomeController = require('../controllers/home_controller');
 var AulasController = require('../controllers/aulas_controller');
 var SobreController = require('../controllers/sobre_controller');
+var VagasController = require('../controllers/vagas_controller');
 
 router.get('/', HomeController.index);
 
 router.get('/aula', AulasController.aula);
 router.get('/aulas', AulasController.index);
+
+router.get('/vagas', VagasController.index);
 
 router.get('/sobre', SobreController.index);
 router.get('/sobre/alunos', SobreController.alunos);
